05368a629d22ca34a565536b38dc8cd7063a63c3
Shreyash Kulkarni <shreyashkulkarniofficial248@gmail.com>
2026-09-16 19:27:02 +0530

    Implemented the Accept connection

--- src/main.cpp
+++ src/main.cpp
@@ -42,8 +42,42 @@ int main() {
         return 1;
     }
 
-    std::cout << "Socket successfully bound to port 6379\n";
+    std::cout << "Socket bound to port 6379\n";
 
+    if (listen(server_fd, SOMAXCONN) == -1) {
+        std::cerr << "listen: "
+                  << std::strerror(errno)
+                  << '\n';
+
+        close(server_fd);
+        return 1;
+    }
+
+    std::cout << "Server is listening on port 6379\n";
+
+    sockaddr_in client_address{};
+    socklen_t client_address_length = sizeof(client_address);
+
+    int client_fd = accept(
+        server_fd,
+        reinterpret_cast<sockaddr*>(&client_address),
+        &client_address_length
+    );
+
+    if (client_fd == -1) {
+        std::cerr << "accept: "
+                  << std::strerror(errno)
+                  << '\n';
+
+        close(server_fd);
+        return 1;
+    }
+
+    std::cout << "Client connected. fd = "
+              << client_fd
+              << '\n';
+
+    close(client_fd);
     close(server_fd);
 
     return 0;
